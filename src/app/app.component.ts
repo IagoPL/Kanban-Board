a22ac87b90802a28b6f33c588e72e2cbd4ac2ca8
IagoPL <iagoprietolamas@gmail.com>
2022-12-13 12:48:59 +0100

    json actualizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,63 @@ export class AppComponent {
   tareas: Tarea[];
 
   constructor() {
+    const tareasJSON2: string = `{
+      "tareas": [
+
+      { "id": 0, 
+      "lista": "${k_FINALIZADAS_LISTA}", 
+      "img":"https://picsum.photos/300/200", 
+      "titulo": "Tarea 1: Diseño UI tiene un usuaiio",
+      "usuarios": [{
+        "email": "lponts@ilerna.com", 
+        "img":"https://picsum.photos/300/300",
+        "nick": "Juan",
+        "alt":"Usuario"
+      }], 
+      "fechaFin": "2019-01-16" },
+
+
+      { "id": 1, 
+      "lista": "${k_PROGRESO_LISTA}", 
+      "img":"https://picsum.photos/300/200", 
+      "titulo": "Tarea 2: Diseño de todo el Backend no tiene usuairo", 
+      "usuarios": [
+
+      ], 
+      "fechaFin": "2022-11-09" },
+
+
+      { "id": 2, 
+      "lista": "${k_PENDIENTES_LISTA}", 
+      "img": null,
+      "titulo": "Tarea 3: Diseño de la base de datos tiene 2 usuarios", 
+      "usuarios":
+      [
+        {"email": "jdominguez@ilerna.com", 
+        "img":"https://picsum.photos/200/200", 
+        "nick": "Jose", 
+        "alt": "Usuario"},
+        
+        { "email": "lponts@ilerna.com", 
+        "img":"https://picsum.photos/100/100", 
+        "nick": "Laura", 
+        "alt":"Usuario"}
+      ], 
+      "fechaFin": "2022-11-16" },
+
+
+      { "id": 3, 
+      "lista": "${k_PENDIENTES_LISTA}",
+      "img": null,
+      "titulo": "Tarea 4: Implementar todo el Front-End no tiene usuario",
+      "usuarios": [
+
+      ],
+      "fechaFin": null }
+
+
+      ]
+      }`;
     const tareasJSON: string = `{
       "tareas": [
         { "lista": "${k_FINALIZADAS_LISTA}", 
@@ -51,7 +108,7 @@ export class AppComponent {
       ]
     }`;
 
-    const tareasDict: any = JSON.parse(tareasJSON);
+    const tareasDict: any = JSON.parse(tareasJSON2);
     this.tareas = tareasDict['tareas'];
 
     this.listas.push(k_PENDIENTES_LISTA);
